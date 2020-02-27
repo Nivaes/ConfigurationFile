eafac282db9f851ab33080dea7c4b9bbda73f197
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-02-27 13:52:28 +0100

    Add project files.

---
+++ ConfigurationFile/Sources/Platforms/Android/ConfigurationStreamProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace Nivaes.ConfigurationFile
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Android.Content;
+    using Android.Content.Res;
+
+    public static class ConfigurationStreamProvider
+    {
+        private static Lazy<Context> mContextLazy;
+
+        public static void Initialize(Func<Context> contextFactory)
+        {
+            mContextLazy = new Lazy<Context>(contextFactory);
+        }
+
+        public static ValueTask<Stream> GetFileStream(string fileName)
+        {
+            if (mContextLazy == null) throw new Exception("ConfigurationStreamProvider not initialize. call o Initialize.");
+
+            AssetManager assets = mContextLazy.Value.Assets;
+
+            var stream = assets.Open(fileName);
+
+            return new ValueTask<Stream>(stream);
+        }
+    }
+}
---
+++ ConfigurationFile/Sources/Platforms/Netstandard/ConfigurationStreamProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Nivaes.ConfigurationFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ConfigurationStreamProvider
+        : IConfigurationStreamProvider
+    {
+        public Task<Stream> GetFileStream(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
